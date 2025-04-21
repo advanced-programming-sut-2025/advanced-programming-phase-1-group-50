97eb4021a6d918fcdc059b577655e0cbe27f43a5
amirmahdi <amirmahdifallah8406@gmail.com>
2025-04-21 18:40:11 +0330

    add "Fruit" file for trees

---
+++ src/main/java/models/foraging/Fruit.java
@@ -0,0 +1,18 @@
+package models.foraging;
+
+public enum Fruit {
+    Apricot,
+    Cherry,
+    Banana,
+    Mango,
+    Orange,
+    Peach,
+    Apple,
+    Pomegranate,
+    OakResin,
+    MapleSyrup,
+    PineTar,
+    Sap,
+    CommonMushroom,
+    MysticSyrup;
+}
