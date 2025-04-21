1e89671035dfb4069262232ce18c99a828cb4ea7
amirmahdi <amirmahdifallah8406@gmail.com>
2025-04-21 22:33:16 +0330

    insert information of "ForagingMinerals" in "ForagingMineral" File and delete previous ForagingMineral

--- src/main/java/models/foraging/ForagingMineral.java
+++ src/main/java/models/foraging/ForagingMineral.java
@@ -1,5 +1,35 @@
 package models.foraging;
 
-public class ForagingMineral {
-    private ForagingMineralsTypes type;
+public enum ForagingMineral {
+    Quartz(25),
+    EarthCrystal(50),
+    FrozenTear(75),
+    FireQuartz(100),
+    Emerald(250),
+    Aquamarine(180),
+    Ruby(250),
+    Amethyst(100),
+    Topaz(80),
+    Jade(200),
+    Diamond(750),
+    PrismaticShard(2000),
+    Copper(5),
+    Iron(10),
+    Gold(25),
+    Iridium(100),
+    Coal(15);
+
+    private final int sellPrice;
+
+    ForagingMineral(int sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public String getName() {
+        return name();
+    }
+
+    public int getSellPrice() {
+        return sellPrice;
+    }
 }
