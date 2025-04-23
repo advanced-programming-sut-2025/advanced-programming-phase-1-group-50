9411e734b67fa5a5b757436bb34b8512c20d58d1
amirmahdi <amirmahdifallah8406@gmail.com>
2025-04-23 09:53:38 +0330

    complete information of AnimalGood and Animal

--- src/main/java/models/animals/Animal.java
+++ src/main/java/models/animals/Animal.java
@@ -1,25 +1,30 @@
 package models.animals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum Animal {
-    Chicken(AnimalType.Coop , 800),
-    Duck(AnimalType.Coop , 1200),
-    Rabbit(AnimalType.Coop , 8000),
-    Dinosaur(AnimalType.Coop , 14000),
-    Cow(AnimalType.Barn , 1500),
-    Goat(AnimalType.Barn , 4000),
-    Sheep(AnimalType.Barn , 8000),
-    Pig(AnimalType.Barn , 16000);
+    Chicken(AnimalType.Coop , 800, new ArrayList<>(Arrays.asList(AnimalGood.Egg, AnimalGood.LargeEgg))),
+    Duck(AnimalType.Coop , 1200, new ArrayList<>(Arrays.asList(AnimalGood.DuckEgg, AnimalGood.DuckFeather))),
+    Rabbit(AnimalType.Coop , 8000, new ArrayList<>(Arrays.asList(AnimalGood.Wool, AnimalGood.RabbitFoot))),
+    Dinosaur(AnimalType.Coop , 14000, new ArrayList<>(List.of(AnimalGood.DinosaurEgg))),
+    Cow(AnimalType.Barn , 1500, new ArrayList<>(Arrays.asList(AnimalGood.Milk, AnimalGood.LargeMilk))),
+    Goat(AnimalType.Barn , 4000, new ArrayList<>(Arrays.asList(AnimalGood.GoatMilk, AnimalGood.LargeGoatMilk))),
+    Sheep(AnimalType.Barn , 8000, new ArrayList<>(List.of(AnimalGood.Wool))),
+    Pig(AnimalType.Barn , 16000, new ArrayList<>(List.of(AnimalGood.Truffle)));
 
 
     private final AnimalType animalType;
     private final int Price;
-    // ArrayList<mahsoolat>
+    private final ArrayList<AnimalGood> animalGoods;
 
 
-     Animal(AnimalType animalType , int price){
+     Animal(AnimalType animalType , int price, ArrayList<AnimalGood> animalGoods){
         this.animalType = animalType;
         this.Price = price;
-    }
+         this.animalGoods = animalGoods;
+     }
 
     public int getPrice() {
         return Price;
@@ -27,5 +32,9 @@ public enum Animal {
 
     public AnimalType getAnimalType() {
         return animalType;
+    }
+
+    public ArrayList<AnimalGood> getAnimalGoods() {
+         return animalGoods;
     }
 }
