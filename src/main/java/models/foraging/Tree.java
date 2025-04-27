f9189f8e805ab29a0d99b886d0c28769fa015a25
amirmahdi <amirmahdifallah8406@gmail.com>
2025-04-27 18:41:26 +0330

    add grow & harvest function for Tree

--- src/main/java/models/foraging/Tree.java
+++ src/main/java/models/foraging/Tree.java
@@ -1,19 +1,59 @@
 package models.foraging;
 
 import models.Placeable;
+import models.app.App;
+import models.date.Time;
 
 import java.awt.*;
 
 public class Tree implements Placeable {
     private TreeType type;
+    private int levelOfGrowth;
+    private Time lastGrowthTime;
+    private Time lastHarvestTime;
     private Rectangle bounds;
+
     public Tree(int x, int y, int width, int height){
         this.bounds = new Rectangle(x, y, width, height);
     }
+
     public Rectangle getBounds() {
         return bounds;
     }
+
     public char getSymbol() {
         return 'T';
+    }
+
+    public void grow() {
+        if (isComplete())
+            return;
+
+        Time today = App.getGame().getTime().clone();
+        int timeForGrow = type.getTimeForGrow(levelOfGrowth);
+
+        if (lastGrowthTime.getDate() + timeForGrow == today.getDate()) {
+            levelOfGrowth++;
+            lastGrowthTime = today;
+        }
+
+    }
+
+    public boolean isComplete() {
+        return levelOfGrowth >= type.getStages().size();
+    }
+
+    public boolean harvest() {
+        if (!isComplete())
+            return false;
+
+        Time today = App.getGame().getTime().clone();
+        int timeForGrow = type.getHarvestCycle();
+
+        if (lastHarvestTime == null || lastHarvestTime.getDate() + timeForGrow <= today.getDate()) {
+            lastHarvestTime = today;
+            return true;
+        }
+        return false;
     }
 }
