66b61f38ef5007f25385e034faf8dc59851097ee
amirmahdi <amirmahdifallah8406@gmail.com>
2025-05-10 20:13:01 +0330

    change how to increase "Ability" level and add recipes to "player"

--- src/main/java/models/userInfo/Ability.java
+++ src/main/java/models/userInfo/Ability.java
@@ -1,5 +1,7 @@
 package models.userInfo;
 
+import models.recipes.CraftingRecipes;
+
 public class Ability {
     private int farmingLevel = 0;
     private int farmingRate = 0;
@@ -9,7 +11,12 @@ public class Ability {
     private int foragingRate = 0;
     private int fishingLevel = 0;
     private int fishingRate = 0;
+    private final Player player;
     private final static int maxLevel = 4;
+
+    public Ability(Player player) {
+        this.player = player;
+    }
 
     public static int getMaxLevel() {
         return maxLevel;
@@ -55,7 +62,7 @@ public class Ability {
         farmingRate += amount;
         while (farmingLevel < maxLevel && farmingRate >= getRequiredPointsForLevel(farmingLevel + 1)) {
             farmingRate -= getRequiredPointsForLevel(farmingLevel + 1);
-            farmingLevel++;
+            increaseFarmingLevel(1);
         }
     }
 
@@ -63,7 +70,7 @@ public class Ability {
         miningRate += amount;
         while (miningLevel < maxLevel && miningRate >= getRequiredPointsForLevel(miningLevel + 1)) {
             miningRate -= getRequiredPointsForLevel(miningLevel + 1);
-            miningLevel++;
+            increaseMiningLevel(1);
         }
     }
 
@@ -71,7 +78,7 @@ public class Ability {
         foragingRate += amount;
         while (foragingLevel < maxLevel && foragingRate >= getRequiredPointsForLevel(foragingLevel + 1)) {
             foragingRate -= getRequiredPointsForLevel(foragingLevel + 1);
-            foragingLevel++;
+            increaseForagingLevel(1);
         }
     }
 
@@ -79,7 +86,49 @@ public class Ability {
         fishingRate += amount;
         while (fishingLevel < maxLevel && fishingRate >= getRequiredPointsForLevel(fishingLevel + 1)) {
             fishingRate -= getRequiredPointsForLevel(fishingLevel + 1);
-            fishingLevel++;
+            increaseFishingLevel(1);
         }
+    }
+
+    public void increaseFarmingLevel(int amount) {
+        farmingLevel += amount;
+        if (farmingLevel == 1) {
+            player.addRecipe(CraftingRecipes.Sprinkler);
+            player.addRecipe(CraftingRecipes.BeeHouse);
+        }
+        else if (farmingLevel == 2) {
+            player.addRecipe(CraftingRecipes.QualitySprinkler);
+            player.addRecipe(CraftingRecipes.DeluxeScarecrow);
+            player.addRecipe(CraftingRecipes.CheesePress);
+            player.addRecipe(CraftingRecipes.PreservesJar);
+        }
+        else if (farmingLevel == 3) {
+            player.addRecipe(CraftingRecipes.IridiumSprinkler);
+            player.addRecipe(CraftingRecipes.OilMaker);
+            player.addRecipe(CraftingRecipes.Keg);
+            player.addRecipe(CraftingRecipes.Loom);
+        }
+    }
+
+    public void increaseMiningLevel(int amount) {
+        miningLevel += amount;
+        if (miningLevel == 1)
+            player.addRecipe(CraftingRecipes.CherryBomb);
+        if (miningLevel == 2)
+            player.addRecipe(CraftingRecipes.Bomb);
+        if (miningLevel == 3)
+            player.addRecipe(CraftingRecipes.MegaBomb);
+    }
+
+    public void increaseForagingLevel(int amount) {
+        foragingLevel += amount;
+        if (foragingLevel == 1)
+            player.addRecipe(CraftingRecipes.CharcoalKiln);
+        if (foragingLevel == 4)
+            player.addRecipe(CraftingRecipes.MysticTreeSeed);
+    }
+
+    public void increaseFishingLevel(int amount) {
+        fishingLevel += amount;
     }
 }
