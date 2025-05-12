6e9b61a53ea86872608f7d0741da753eee678e30
MmdHsMjdn <mohammad.h060684@gmail.com>
2025-05-12 23:22:18 +0330

    adding stroe items for Fishshop

---
+++ src/main/java/models/stores/FishShopCraftingRecipe.java
@@ -0,0 +1,18 @@
+package models.stores;
+
+import models.recipes.CraftingRecipes;
+
+public class FishShopCraftingRecipe extends ShopItem{
+
+    private final CraftingRecipes recipe;
+
+
+    public FishShopCraftingRecipe(String name, CraftingRecipes recipe,int price, int dailyLimit) {
+        super(name, price, dailyLimit);
+        this.recipe = recipe;
+    }
+
+    public CraftingRecipes getRecipe() {
+        return recipe;
+    }
+}
