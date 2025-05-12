33e98a30d15fa6a1050d2ec8bea1b6ea7b0ee51f
amirmahdi <amirmahdifallah8406@gmail.com>
2025-05-12 22:04:39 +0330

    add controllers for Animals, Cooking, Crafting, Foraging

---
+++ src/main/java/controller/CookingAndCraftingControllers/CraftingController.java
@@ -0,0 +1,90 @@
+package controller.CookingAndCraftingControllers;
+
+import models.Result;
+import models.app.App;
+import models.cooking.Food;
+import models.foraging.TreeSource;
+import models.manuFactor.ArtisanMachine;
+import models.manuFactor.Ingredient;
+import models.recipes.CookingRecipe;
+import models.recipes.CraftingRecipes;
+import models.userInfo.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CraftingController {
+
+    public Result craftingShowRecipes() {
+        Player player = App.getGame().getCurrentPlayingPlayer();
+        ArrayList<CraftingRecipes> recipes = player.getBackpack().getCraftingRecipes();
+        StringBuilder output = new StringBuilder();
+        output.append("Crafting Recipes: \n");
+        for (int i = 0; i < recipes.size(); i++) {
+            output.append(i+1).append(". ").append(recipes.get(i).toString()).append("\n");
+        }
+        return new Result(true, output.toString());
+    }
+
+    public Result craftingCraft(String ItemName) {
+        CraftingRecipes recipe = CraftingRecipes.getRecipeByName(ItemName);
+        Player player = App.getGame().getCurrentPlayingPlayer();
+
+        if (recipe == null)
+            return new Result(false, "Recipe <" + ItemName + "> not found!");
+        if (!player.getBackpack().containRecipe(recipe))
+            return new Result(false, "You don't have <" + ItemName + "> CraftingRecipe in your backpack!");
+        if (player.getBackpack().hasCapacity())
+            return new Result(false, "You don't have enough space in backpack!");
+
+        HashMap<Ingredient, Integer> ingredients = recipe.getIngredients();
+        for (Ingredient ingredient : ingredients.keySet()) {
+            if (!(player.getBackpack().getIngredientQuantity().containsKey(ingredient) &&
+                    player.getBackpack().getIngredientQuantity().get(ingredient) >= ingredients.get(ingredient))) {
+                return new Result(false, "You don't have enough <" + ingredient + "> in your backpack!");
+            }
+            player.getBackpack().removeIngredients(ingredient, ingredients.get(ingredient));
+        }
+
+        ArtisanMachine artisanMachine = ArtisanMachine.getArtisanMachineByRecipe(recipe);
+        if (artisanMachine != null)
+            player.getBackpack().addArtisanMachine(artisanMachine);
+        if (recipe.equals(CraftingRecipes.MysticTreeSeed))
+            player.getBackpack().addIngredients(TreeSource.MysticTreeSeeds, 1);
+        //TODO else
+
+        player.consumeEnergy(2);
+
+        return new Result(true, "You craft <" + ItemName + "> successfully!");
+    }
+
+    public Result addItem(String ItemName, int quantity) {
+        Player player = App.getGame().getCurrentPlayingPlayer();
+
+        if (quantity <= 0)
+            return new Result(false, "The quantity must be greater than zero!");
+        if (!player.getBackpack().hasCapacity(quantity))
+            return new Result(false, "You don't have enough space in backpack!");
+        CraftingRecipes craftingRecipe = CraftingRecipes.getRecipeByName(ItemName);
+        ArtisanMachine machine;
+        if (craftingRecipe != null) {
+            if ((machine = ArtisanMachine.getArtisanMachineByRecipe(craftingRecipe)) != null) {
+                player.getBackpack().addArtisanMachine(machine);
+            }
+            else if (craftingRecipe.equals(CraftingRecipes.MysticTreeSeed)) {
+                player.getBackpack().addIngredients(TreeSource.MysticTreeSeeds, quantity);
+            }
+            //TODO else
+            return new Result(true, "You add <" + ItemName + "> successfully!");
+        }
+        CookingRecipe cookingRecipe = CookingRecipe.getRecipeByName(ItemName);
+        if (cookingRecipe != null) {
+            Food food = Food.getFoodByName(ItemName);
+            player.getBackpack().addIngredients(food, quantity);
+            return new Result(true, "You add <" + ItemName + "> successfully!");
+        }
+        //TODO else item
+        return new Result(false, "There is no such Item!");
+    }
+
+}
