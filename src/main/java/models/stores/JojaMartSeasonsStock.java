8dbf478d8adcfcd8a5e21f264624fc0b4b260f8a
MmdHsMjdn <mohammad.h060684@gmail.com>
2025-05-13 19:23:10 +0330

    adding JojaMart Items

--- src/main/java/models/stores/JojaMartSeasonsStock.java
+++ src/main/java/models/stores/JojaMartSeasonsStock.java
@@ -1,17 +1,24 @@
 package models.stores;
 
+import models.date.Season;
 import models.foraging.Seeds;
 
 public class JojaMartSeasonsStock extends ShopItem{
 
     private final Seeds seedType;
+    private final Season season;
 
-    public JojaMartSeasonsStock(String name, Seeds seedType ,int price, int dailyLimit) {
+    public JojaMartSeasonsStock(String name, Seeds seedType , Season season, int price, int dailyLimit) {
         super(name, price, dailyLimit);
         this.seedType = seedType;
+        this.season = season;
     }
 
     public Seeds getSeedType() {
         return seedType;
+    }
+
+    public Season getSeason() {
+        return season;
     }
 }
