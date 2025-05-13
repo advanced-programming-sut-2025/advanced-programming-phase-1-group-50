b23127642a189a269ad08b240b5b5fd0471e5622
MmdHsMjdn <mohammad.h060684@gmail.com>
2025-05-13 11:40:34 +0330

    adding MarnieRanch Store Loadinventory

--- src/main/java/models/stores/MarnieRanch.java
+++ src/main/java/models/stores/MarnieRanch.java
@@ -1,5 +1,9 @@
 package models.stores;
 
+import models.animals.AnimalType;
+import models.animals.HabitatSize;
+import models.animals.HabitatType;
+
 import java.awt.*;
 import java.util.ArrayList;
 
@@ -20,6 +24,18 @@ public class MarnieRanch extends Store {
     @Override
     public void loadInventory() {
         
+        inventory.add(new MarnieRanchLiveStockItem("Chicken", AnimalType.Chicken, HabitatType.Coop , HabitatSize.Regular, 800 ,2));
+        inventory.add(new MarnieRanchLiveStockItem("Cow", AnimalType.Cow, HabitatType.Barn , HabitatSize.Regular, 1500 ,2));
+        inventory.add(new MarnieRanchLiveStockItem("Goat", AnimalType.Goat, HabitatType.Barn , HabitatSize.Big, 4000 ,2));
+        inventory.add(new MarnieRanchLiveStockItem("Duck", AnimalType.Duck, HabitatType.Coop , HabitatSize.Big, 1200 ,2));
+        inventory.add(new MarnieRanchLiveStockItem("Sheep", AnimalType.Sheep, HabitatType.Barn , HabitatSize.Deluxe, 8000 ,2));
+        inventory.add(new MarnieRanchLiveStockItem("Rabbit", AnimalType.Rabbit, HabitatType.Coop , HabitatSize.Deluxe, 8000 ,2));
+        inventory.add(new MarnieRanchLiveStockItem("Dinosaur", AnimalType.Dinosaur, HabitatType.Coop , HabitatSize.Big, 14000 ,2));
+        inventory.add(new MarnieRanchLiveStockItem("Pig", AnimalType.Pig, HabitatType.Barn , HabitatSize.Deluxe, 16000 ,2));
+        inventory.add(new ShopItem("Hay",50,Integer.MAX_VALUE));
+        inventory.add(new ShopItem("Milk Pail",1000,1));
+        inventory.add(new ShopItem("Shears",1000,1));
+
     }
 
 //    @Override
