fe26dff565cb4cd4a533afe152fceaa917cb5111
amirmahdi <amirmahdifallah8406@gmail.com>
2025-05-15 20:24:28 +0330

    adding initialize complete for "seeds" and "TreeSource"

--- src/main/java/models/foraging/Seeds.java
+++ src/main/java/models/foraging/Seeds.java
@@ -50,13 +50,58 @@ public enum Seeds implements Ingredient {
     MixedSeeds(Season.Special, null);
 
     private final Season season;
-    private final CropType crop;
+    private CropType crop;
     private final static HashMap<String, Seeds> stringToSeeds = new HashMap<>();
 
     static {
         for (Seeds value : Seeds.values()) {
             stringToSeeds.put(value.name().toLowerCase(), value);
         }
+    }
+
+    public static void completeInitialize() {
+        JazzSeeds.crop = CropType.BlueJazz;
+        CarrotSeeds.crop = CropType.Carrot;
+        CauliflowerSeeds.crop = CropType.Cauliflower;
+        CoffeeBean.crop = CropType.CoffeeBean;
+        GarlicSeeds.crop = CropType.Garlic;
+        BeanStarter.crop = CropType.GreenBean;
+        KaleSeeds.crop = CropType.Kale;
+        ParsnipSeeds.crop = CropType.Parsnip;
+        PotatoSeeds.crop = CropType.Potato;
+        RhubarbSeeds.crop = CropType.Rhubarb;
+        StrawberrySeeds.crop = CropType.Strawberry;
+        TulipBulb.crop = CropType.Tulip;
+        RiceShoot.crop = CropType.UnMilledRice;
+        BlueberrySeeds.crop = CropType.Blueberry;
+        CornSeeds.crop = CropType.Corn;
+        HopsStarter.crop = CropType.Hops;
+        PepperSeeds.crop = CropType.HotPepper;
+        MelonSeeds.crop = CropType.Melon;
+        PoppySeeds.crop = CropType.Poppy;
+        RadishSeeds.crop = CropType.Radish;
+        RedCabbageSeeds.crop = CropType.RedCabbage;
+        StarfruitSeeds.crop = CropType.Starfruit;
+        SpangleSeeds.crop = CropType.SummerSpangle;
+        SummerSquashSeeds.crop = CropType.SummerSquash;
+        SunflowerSeeds.crop = CropType.Sunflower;
+        TomatoSeeds.crop = CropType.Tomato;
+        WheatSeeds.crop = CropType.Wheat;
+        AmaranthSeeds.crop = CropType.Amaranth ;
+        ArtichokeSeeds.crop = CropType.Artichoke;
+        BeetSeeds.crop = CropType.Beet;
+        BokChoySeeds.crop = CropType.BokChoy;
+        BroccoliSeeds.crop = CropType.Broccoli;
+        CranberrySeeds.crop = CropType.Cranberries;
+        EggplantSeeds.crop = CropType.Eggplant;
+        FairySeeds.crop = CropType.FairyRose;
+        GrapeStarter.crop = CropType.Grape;
+        PumpkinSeeds.crop = CropType.Pumpkin;
+        YamSeeds.crop = CropType.Yam;
+        RareSeed.crop = CropType.SweetGemBerry;
+        PowdermelonSeeds.crop = CropType.PowderMelon;
+        AncientSeeds.crop = CropType.AncientFruit;
+
     }
 
     Seeds(Season season, CropType crop) {
