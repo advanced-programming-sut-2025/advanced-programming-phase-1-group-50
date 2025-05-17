c81118fb9ac1183b6638bb133f76c863b5363e0a
amirmahdi <amirmahdifallah8406@gmail.com>
2025-05-17 14:56:52 +0330

    add ability controller

---
+++ src/main/java/controller/AbilityAndEnergyController/AbilityController.java
@@ -0,0 +1,31 @@
+package controller.AbilityAndEnergyController;
+
+import models.Result;
+import models.app.App;
+import models.userInfo.Ability;
+
+public class AbilityController {
+    public Result showFarmingAbility() {
+        Ability ability = App.getGame().getCurrentPlayingPlayer().getAbility();
+        return new Result(true , String.format("your farming ability details is: \nLevel: %d \nRate: %d"
+                , ability.getFarmingLevel(), ability.getFarmingRate()));
+    }
+
+    public Result showForagingAbility() {
+        Ability ability = App.getGame().getCurrentPlayingPlayer().getAbility();
+        return new Result(true , String.format("your foraging ability details is: \nLevel: %d \nRate: %d"
+                , ability.getForagingLevel(), ability.getForagingRate()));
+    }
+
+    public Result showMiningAbility() {
+        Ability ability = App.getGame().getCurrentPlayingPlayer().getAbility();
+        return new Result(true , String.format("your farming ability details is: \nLevel: %d \nRate: %d"
+                , ability.getMiningLevel(), ability.getMiningRate()));
+    }
+
+    public Result showFishingAbility() {
+        Ability ability = App.getGame().getCurrentPlayingPlayer().getAbility();
+        return new Result(true , String.format("your farming ability details is: \nLevel: %d \nRate: %d"
+                , ability.getFishingLevel(), ability.getFishingRate()));
+    }
+}
