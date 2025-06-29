1a3b2f8c7ad2062ce6c05df69930fececf87003e
morez <mohammadrezagh748@gmail.com>
2025-06-29 21:42:36 +0330

    add assets

---
+++ core/src/main/java/com/stardew/models/GameAssetManagers/GamePictureManager.java
@@ -0,0 +1,101 @@
+package com.stardew.models.GameAssetManagers;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class GamePictureManager {
+    public static Skin skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+
+
+    //Tools :
+        //Axe :
+
+            public static Texture axeTexture = new Texture("Tools/Axe/Axe.png");
+            public static Texture steelAxeTexture = new Texture("Tools/Axe/Steel_Axe.png");
+            public static Texture copperAxeTexture = new Texture("Tools/Axe/Copper_Axe.png");
+            public static Texture goldAxeTexture = new Texture("Tools/Axe/Gold_Axe.png");
+            public static Texture iridiumAxeTexture = new Texture("Tools/Axe/Iridium_Axe.png");
+
+        //Pickaxe :
+
+            public static Texture pickaxeTexture = new Texture("Tools/Pickaxe/Pickaxe.png");
+            public static Texture steelPickaxeTexture = new Texture("Tools/Pickaxe/Steel_Pickaxe.png");
+            public static Texture copperPickaxeTexture = new Texture("Tools/Pickaxe/Copper_Pickaxe.png");
+            public static Texture goldPickaxeTexture = new Texture("Tools/Pickaxe/Gold_Pickaxe.png");
+            public static Texture iriduimPiclaxeTexture = new Texture("Tools/Pickaxe/Iridium_Pickaxe.png");
+
+        //Scythe :
+
+            public static Texture scytheTexture = new Texture("Tools/Scythe.png");
+            public static Texture goldScytheTexture = new Texture("Tools/Golden_Scythe.png");
+            public static Texture iridiumScytheTexture = new Texture("Tools/Iridium_Scythe.png");
+
+        //Backpack :
+
+            public static Texture backpackTexture = new Texture("Tools/Backpack.png");
+            public static Texture backpack36Texture = new Texture("Tools/36_Backpack.png");
+
+        //TrashCan :
+
+            public static Texture copperTrashCanTexture = new Texture("Tools/Trash_Can_Copper.png");
+            public static Texture goldTrashCanTexture = new Texture("Tools/Trash_Can_Gold.png");
+            public static Texture steelTrashCanTexture = new Texture("Tools/Trash_Can_Steel.png");
+            public static Texture iridiumTrashCanTexture = new Texture("Tools/Trash_Can_Iridium.png");
+
+        //MilkPail :
+
+            public static Texture milkPailTexture = new Texture("Tools/Milk_Pail.png");
+
+        //Shear :
+
+            public static Texture shearTexture = new Texture("Tools/Shears.png");
+
+        //wateringCan :
+
+            public static Texture wateringCanTexture = new Texture("Tools/Watering_Can/Watering_Can.png");
+            public static Texture copperWateringCanTexture = new Texture("Tools/Watering_Can/Copper_Watering_Can.png");
+            public static Texture goldWateringCanTexture = new Texture("Tools/Watering_Can/Gold_Watering_Can.png");
+            public static Texture steelWateringCanTexture = new Texture("Tools/Watering_Can/Steel_Watering_Can.png");
+            public static Texture iridiumWateringCanTexture = new Texture("Tools/Watering_Can/Iridium_Watering_Can.png");
+
+    //Villagers :
+
+        public static Texture abigailTexture = new Texture("Villages/Abigail.png");
+        public static Texture harveyTexture = new Texture("Villages/Harvey.png");
+        public static Texture leahTexture = new Texture("Villages/Leah.png");
+        public static Texture marnieTexture = new Texture("Villages/Marnie.png");
+        public static Texture pierreTexture = new Texture("Villages/Pierre.png");
+        public static Texture robinTexture = new Texture("Villages/Robin.png");
+        public static Texture sebastianTexture = new Texture("Villages/Sebastian.png");
+        public static Texture willyTexture = new Texture("Villagers/Willy.png");
+
+    //Rock :
+
+        public static Texture farmBoulderTexture = new Texture("Rock/Farm_Boulder.png");
+        public static Texture MineBoulderTexture1 = new Texture("Rock/Mine_Boulder_1.png");
+        public static Texture mineBoulderTexture2 = new Texture("Rock/Mine_Boulder_2.png");
+        public static Texture mineBoulderTexture3 = new Texture("Rock/Mine_Boulder_3.png");
+        public static Texture mineBoulderTexture4 = new Texture("Rock/Mine_Boulder_4.png");
+        public static Texture quarryBoulderTexture = new Texture("Rock/Quarry_Boulder.png");
+        public static Texture stoneIndex32Texture = new Texture("Rock/Stone_Index32.png");
+        public static Texture stoneIndex34Texture = new Texture("Rock/Stone_Index34.png");
+        public static Texture stoneIndex36Texture = new Texture("Rock/Stone_Index36.png");
+        public static Texture stoneIndex38Texture = new Texture("Rock/Stone_Index38.png");
+        public static Texture stoneIndex40Texture = new Texture("Rock/Stone_Index40.png");
+        public static Texture stoneIndex42Texture = new Texture("Rock/Stone_Index42.png");
+        public static Texture stoneIndex48Texture = new Texture("Rock/Stone_Index48.png");
+        public static Texture stoneIndex50Texture = new Texture("Rock/Stone_Index50.png");
+        public static Texture stoneIndex52Texture = new Texture("Rock/Stone_Index52.png");
+        public static Texture stoneIndex54Texture = new Texture("Rock/Stone_Index54.png");
+        public static Texture stoneIndex56Texture = new Texture("Rock/Stone_Index56.png");
+        public static Texture stoneIndex58Texture = new Texture("Rock/Stone_Index58.png");
+        public static Texture stoneIndex343Texture = new Texture("Rock/Stone_Index343.png");
+        public static Texture stoneIndex450Texture = new Texture("Rock/Stone_Index450.png");
+        public static Texture stoneIndex668Texture = new Texture("Rock/Stone_Index668.png");
+        public static Texture stoneIndex670Texture = new Texture("Rock/Stone_Index670.png");
+
+
+
+
+}
