19809126bae47824e8e02dca21eac1f84c0aba14
amirmahdi <amirmahdifallah8406@gmail.com>
2025-07-10 23:42:20 +0330

    add stage for gameScreen, change InputProcessor to inputMultiplexer, initialize SmartTooltip

--- core/src/main/java/com/stardew/view/GameScreenMenu.java
+++ core/src/main/java/com/stardew/view/GameScreenMenu.java
@@ -1,19 +1,23 @@
 package com.stardew.view;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.stardew.controller.PlayerController;
+import com.stardew.models.GameAssetManagers.GamePictureManager;
 import com.stardew.models.animals.GameModel;
 import com.stardew.models.app.App;
+import com.stardew.view.windows.SmartTooltip;
 
 public class GameScreenMenu implements Screen {
 
     private GameModel gameModel;
     private GameRenderer gameRenderer;
     private GameMenuInputAdapter gameMenuInputAdapter;
-    private PlayerController playerController;
+    private Stage stage;
 
     public GameScreenMenu(){
         initializeGame();
@@ -22,11 +26,17 @@ public class GameScreenMenu implements Screen {
     public void initializeGame(){
 
         gameModel = new GameModel(App.getGame().getMap() , 250 , 200);
-        playerController = new PlayerController(App.getGame().getCurrentPlayingPlayer(), gameModel);
-        gameModel.setPlayerController(playerController);
+        gameModel.setPlayerController(new PlayerController(App.getGame().getCurrentPlayingPlayer(), gameModel));
         gameMenuInputAdapter = new GameMenuInputAdapter(gameModel);
         gameRenderer = new GameRenderer(gameModel , gameMenuInputAdapter);
-        Gdx.input.setInputProcessor(gameMenuInputAdapter);
+
+        stage = new Stage(new ScreenViewport(gameModel.getCamera()));
+        InputMultiplexer inputMultiplexer = new InputMultiplexer();
+        inputMultiplexer.addProcessor(stage);
+        inputMultiplexer.addProcessor(gameMenuInputAdapter);
+        Gdx.input.setInputProcessor(inputMultiplexer);
+
+        SmartTooltip.initialize(stage, GamePictureManager.skin);
 
     }
 
@@ -45,6 +55,8 @@ public class GameScreenMenu implements Screen {
         gameRenderer.render();
         gameMenuInputAdapter.update(v);
 
+        stage.act(v);
+        stage.draw();
     }
 
     @Override
