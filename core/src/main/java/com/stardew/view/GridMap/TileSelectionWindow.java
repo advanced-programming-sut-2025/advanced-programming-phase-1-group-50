f88a4d69aca87a74b28cea1abfe258ec26ee8814
amirmahdi <amirmahdifallah8406@gmail.com>
2025-07-13 11:53:27 +0330

    develop GridMapActor to be ability to select an area with width and height, Add controller for it, Add callback while closing

--- core/src/main/java/com/stardew/view/GridMap/TileSelectionWindow.java
+++ core/src/main/java/com/stardew/view/GridMap/TileSelectionWindow.java
@@ -1,22 +1,33 @@
 package com.stardew.view.GridMap;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.stardew.controller.CookingAndCraftingControllers.TileSelectionController;
 import com.stardew.models.GameAssetManagers.GamePictureManager;
+import com.stardew.models.Result;
+import com.stardew.models.mapInfo.Tile;
 import com.stardew.view.windows.CloseableWindow;
 
+import java.util.function.Consumer;
+
 public class TileSelectionWindow extends CloseableWindow {
+    private final TileSelectionController controller;
+    private Consumer<Tile> onCloseCallback;
     private final ImageButton OKButton;
     private final GridMapActor gridMap;
     private final ScrollPane scrollPane;
 
 
-    public TileSelectionWindow(Stage stage) {
-        super(" Select a tile to place machine: ", stage);
+    public TileSelectionWindow(Stage stage, int selectionWidth, int selectionHeight) {
+        super(" Select a tile to place: A Rectangle " + selectionWidth + " x " + selectionHeight, stage);
 
         Label titleLabel = getTitleLabel();
         Label.LabelStyle titleLabelStyle = titleLabel.getStyle();
@@ -32,7 +43,8 @@ public class TileSelectionWindow extends CloseableWindow {
         setColor(Color.BROWN);
 
 
-        gridMap = new GridMapActor();
+        controller = new TileSelectionController(selectionWidth, selectionHeight);
+        gridMap = new GridMapActor(selectionWidth, selectionHeight);
         scrollPane = new ScrollPane(gridMap, GamePictureManager.skin);
         scrollPane.setFadeScrollBars(false);
         scrollPane.setScrollingDisabled(false, false);
@@ -53,5 +65,37 @@ public class TileSelectionWindow extends CloseableWindow {
         getTitleTable().add(titleLabel).expandX().left();
         getTitleTable().add(OKButton).padRight(5).right();
         getTitleTable().add(closeButton).right();
+
+        OKButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Result result = controller.checkTileSelection(gridMap.getSelectedTile());
+                if (result.getSuccessful())
+                    closeWindow();
+                else
+                    showResult(result);
+            }
+        });
+    }
+
+    public void setOnCloseCallback(Consumer<Tile> callback) {
+        this.onCloseCallback = callback;
+    }
+
+    @Override
+    protected void closeWindow() {
+        getChildren().forEach(Actor::clearListeners);
+
+        if (onCloseCallback != null)
+            onCloseCallback.accept(controller.getSelectedTile());
+
+        addAction(Actions.sequence(
+            Actions.parallel(
+                Actions.fadeOut(0.3f),
+                Actions.scaleTo(0.7f, 0.7f, 0.3f)
+            ),
+            Actions.removeActor()
+        ));
+
     }
 }
