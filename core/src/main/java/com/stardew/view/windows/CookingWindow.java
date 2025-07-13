adf788cfb5f91c59abb31f078afc6a53254beacf
amirmahdi <amirmahdifallah8406@gmail.com>
2025-07-13 11:48:31 +0330

    complete Cooking_Foods process and fix bugs.

--- core/src/main/java/com/stardew/view/windows/CookingWindow.java
+++ core/src/main/java/com/stardew/view/windows/CookingWindow.java
@@ -7,12 +7,15 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.stardew.controller.CookingAndCraftingControllers.CookingController;
 import com.stardew.models.GameAssetManagers.CookingAsset;
+import com.stardew.models.Result;
+import com.stardew.models.app.App;
+import com.stardew.models.userInfo.Player;
 
 import java.util.HashMap;
 
 public class CookingWindow extends CloseableWindow {
-    private HashMap<CookingAsset, ImageButton> buttons = new HashMap<>();
-    private CookingController controller = new CookingController();
+    private final HashMap<CookingAsset, ImageButton> buttons = new HashMap<>();
+    private final CookingController controller = new CookingController();
 
     public CookingWindow(Stage stage) {
         super("Cooking Menu", stage);
@@ -42,23 +45,22 @@ public class CookingWindow extends CloseableWindow {
             buttons.put(cookingAsset, new ImageButton(cookingAsset.getStyle()));
         }
 
-        //Player player = App.getGame().getCurrentPlayingPlayer();
+        Player player = App.getGame().getCurrentPlayingPlayer();
 
         for (CookingAsset cookingAsset : CookingAsset.values()) {
             ImageButton imageButton = buttons.get(cookingAsset);
-            //imageButton.setDisabled(!player.getBackpack().containRecipe(cookingAsset.getRecipe()));
-            imageButton.setDisabled(true);  //delete this line
+            imageButton.setDisabled(!player.getBackpack().containRecipe(cookingAsset.getRecipe()));
+
             SmartTooltip tooltip = SmartTooltip.getInstance();
 
             imageButton.addListener(new InputListener() {
                 @Override
                 public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                    imageButton.setDisabled(false);  //delete this line
-//                    if (!imageButton.isDisabled()) {
-//                        Result result = controller.cookingPrepare(cookingAsset.name());
-//                        showResult(result);
-//                        return true;
-//                    }
+                    if (!imageButton.isDisabled()) {
+                        Result result = controller.cookingPrepare(cookingAsset.getRecipe());
+                        showResult(result);
+                        return true;
+                    }
                     return false;
                 }
 
