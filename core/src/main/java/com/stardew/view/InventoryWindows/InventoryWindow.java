ce7c2ed7bd13db208b7d3cd56d69acc2d78d2a5c
amirmahdi <amirmahdifallah8406@gmail.com>
2025-07-16 00:00:32 +0330

    change design of inventory and refrigerator scrollPane and fix some bugs

--- core/src/main/java/com/stardew/view/InventoryWindows/InventoryWindow.java
+++ core/src/main/java/com/stardew/view/InventoryWindows/InventoryWindow.java
@@ -1,11 +1,9 @@
 package com.stardew.view.InventoryWindows;
 
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.stardew.models.GameAssetManagers.GamePictureManager;
 import com.stardew.models.InventoryItem;
@@ -18,7 +16,6 @@ import com.badlogic.gdx.graphics.Color;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class InventoryWindow extends CloseableWindow {
@@ -31,31 +28,25 @@ public class InventoryWindow extends CloseableWindow {
     private final ImageButton trashButton;
     private final TextButton shuffleButton;
     private final HotBarActor hotBar;
-
-
-
-
     private final BackpackGridActor backpackGrid;
-
     private final ScrollPane backpackScrollPane;
-    private final ImageButton OKButton;
+
 
     public InventoryWindow(Stage stage , HotBarActor hotBar) {
         super("Inventory", stage);
         this.hotBar = hotBar;
 
-        Label titleLabel = getTitleLabel();
 
-        setSize(600, 400);
+        setSize(700, 500);
         setPosition(
             stage.getCamera().position.x - getWidth() / 2,
             stage.getCamera().position.y - getHeight() / 2);
-        pad(15, 10, 15, 10);
+        pad(25, 10, 15, 0);
         setColor(Color.ORANGE);
         align(Align.top);
 
         Label.LabelStyle st = new Label.LabelStyle();
-        st.font = new BitmapFont();
+        st.font = GamePictureManager.smallFont;
         st.fontColor = Color.BLACK;
         playerNameLabel = new Label("" , st);
         playerNameLabel.setFontScale(1.5f);
@@ -67,7 +58,6 @@ public class InventoryWindow extends CloseableWindow {
 
 
         backpackGrid = new BackpackGridActor();
-        backpackGrid.setSize(500, 400);
 
         backpackScrollPane = new ScrollPane(backpackGrid, GamePictureManager.skin);
         backpackScrollPane.setFadeScrollBars(false);
@@ -75,25 +65,11 @@ public class InventoryWindow extends CloseableWindow {
         backpackScrollPane.setOverscroll(true, true);
 
 
-        add(backpackScrollPane).height(5 * 40f + 10).width(10 * 40f + 10).padTop(10).center().top();
+        add(backpackScrollPane).height(210).width(505).padTop(50).padBottom(10).center().top();
         row();
+        backpackScrollPane.layout();
+        backpackScrollPane.setScrollPercentY(1f);
 
-
-
-
-        ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
-        style.imageUp = GamePictureManager.OKButtonUp;
-        style.imageOver = new TextureRegionDrawable(GamePictureManager.OKButtonDown);
-        style.imageOver.setMinWidth(32);
-        style.imageOver.setMinHeight(30);
-        style.imageDown = GamePictureManager.OKButtonDown;
-        OKButton = new ImageButton(style);
-
-        getTitleTable().clear();
-        getTitleTable().left();
-        getTitleTable().add(titleLabel).expandX().left();
-        getTitleTable().add(OKButton).padRight(5).right();
-        getTitleTable().add(closeButton).right();
 
         trashButton = new ImageButton(GamePictureManager.trashDrawable);
         trashButton.setSize(50 , 70);
@@ -102,7 +78,7 @@ public class InventoryWindow extends CloseableWindow {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 int xx = backpackGrid.getSelectedX();
                 int yy = backpackGrid.getSelectedY();
-                if(xx == -1 && yy ==     -1) {
+                if(xx == -1 && yy == -1) {
                     return false;
                 }
 
@@ -180,11 +156,11 @@ public class InventoryWindow extends CloseableWindow {
         buttonRowTable.add(socialButton).width(50).height(70).padRight(10);
         buttonRowTable.add(skillsButton).width(50).height(70).padRight(10);
         buttonRowTable.add(mapButton).width(50).height(70).padRight(10);
-        buttonRowTable.add(exitButton).width(50).height(70);
+        buttonRowTable.add(exitButton).width(50).height(70).padRight(10);
         buttonRowTable.add(shuffleButton).width(120).height(70);
 
         row();
-        add(buttonRowTable).padTop(10).left();
+        add(buttonRowTable).padTop(10).center();
 
 
 
