bb94820cf10a1b5c77abdf8859c4eb7984835964
MmdHsMjdn <mohammad.h060684@gmail.com>
2025-07-18 16:40:22 +0330

    completing GiftMenuWindow

--- core/src/main/java/com/stardew/view/PlayersRelationsWindows/GiftHistoryWindow.java
+++ core/src/main/java/com/stardew/view/PlayersRelationsWindows/GiftHistoryWindow.java
@@ -1,10 +1,11 @@
 package com.stardew.view.PlayersRelationsWindows;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.stardew.models.userInfo.Player;
 import com.stardew.view.windows.CloseableWindow;
 
 public class GiftHistoryWindow extends CloseableWindow {
-    public GiftHistoryWindow(Stage stage) {
+    public GiftHistoryWindow(Stage stage, Player player) {
         super("Gifts' history", stage);
     }
 }
