d230277343e80964952633f645167c12be65f407
amirmahdi <amirmahdifallah8406@gmail.com>
2025-07-18 22:53:32 +0330

    some change and fixing bugs in FishBehavior algorithms

--- core/src/main/java/com/stardew/models/animals/FishBehavior/DartBehavior.java
+++ core/src/main/java/com/stardew/models/animals/FishBehavior/DartBehavior.java
@@ -4,23 +4,34 @@ import com.stardew.models.animals.Fish;
 
 public class DartBehavior extends FishBehavior {
     private int direction = 0;
+    private float epsilon = 0.01f;
 
     public DartBehavior() {
-        this.speedUp = 60f;
-        this.speedDown = 60f;
+        this.speedUp = 250f;
+        this.speedDown = 250f;
     }
 
     @Override
     public void update(Fish fish, float delta) {
         elapsedTime += delta;
-        if (elapsedTime >= 0.3f) {
+        if (elapsedTime >= 2f) {
             elapsedTime = 0;
             direction = (int) (Math.random() * 3);
         }
+
+        float previousY = fish.getPosition().y;
 
         switch (direction) {
             case 1 -> fish.moveY(speedUp * delta);
             case 2 -> fish.moveY(-speedDown * delta);
         }
+
+        float newY = fish.getPosition().y;
+
+        if (direction != 0 && Math.abs(newY - previousY) < epsilon) {
+            elapsedTime = 0;
+            direction = (int) (Math.random() * 3);
+        }
+
     }
 }
--- core/src/main/java/com/stardew/models/animals/FishBehavior/FloaterBehavior.java
+++ core/src/main/java/com/stardew/models/animals/FishBehavior/FloaterBehavior.java
@@ -4,9 +4,10 @@ import com.stardew.models.animals.Fish;
 
 public class FloaterBehavior extends FishBehavior {
     private int state;
-    public FloaterBehavior() {
-        this.speedUp = 30f;
 
+    public FloaterBehavior() {
+        this.speedUp = 350f;
+        this.speedDown = 150f;
     }
 
 
@@ -14,17 +15,15 @@ public class FloaterBehavior extends FishBehavior {
     public void update(Fish fish, float delta) {
         elapsedTime += delta;
 
-
         if (elapsedTime >= 1f) {
             elapsedTime = 0;
-            state = (int) (Math.random() * 2);
+            state = (int) (Math.random() * 3);
         }
 
-
-
-
-        if (state == 1 ) {
-            fish.moveY(speedUp * delta);
+        switch (state) {
+            case 1 -> fish.moveY(speedUp * delta);
+            case 2 -> fish.moveY(-speedDown * delta);
         }
+
     }
 }
