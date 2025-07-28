5f33a5e5f94e6447eb2feccebf5b072093647dd0
morez <mohammadrezagh748@gmail.com>
2025-07-28 14:01:09 +0330

    trying to add all of textureRegions to GameAssetIDManager :{

--- client/src/main/java/com/stardew/models/GameAssetManagers/GameAssetIDManager.java
+++ client/src/main/java/com/stardew/models/GameAssetManagers/GameAssetIDManager.java
@@ -1,8 +1,10 @@
 package com.stardew.models.GameAssetManagers;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.stardew.models.date.Season;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class GameAssetIDManager {
     public static HashMap<Integer , TextureRegion> ids = new HashMap<>();
@@ -65,6 +67,130 @@ public class GameAssetIDManager {
         ids.put(53, GamePictureManager.iridiumBarTexture);
         ids.put(54, GamePictureManager.copperBarTexture);
         ids.put(55, GamePictureManager.goldBarTexture);
+
+        //apricot :
+        ids.put(60 , GamePictureManager.apricotSaplingTexture);  //sapling
+        ids.put(61 , GamePictureManager.apricotStage1Texture);
+        ids.put(62 , GamePictureManager.apricotStage2Texture);
+        ids.put(63 , GamePictureManager.apricotStage3Texture);
+        ids.put(64 , GamePictureManager.apricotStage4Texture);
+        ids.put(65  , GamePictureManager.apricotStage5WithFruit);
+        int base = 0 ;
+        for(Map.Entry<Season, TextureRegion> entry : GamePictureManager.apricotStage5.entrySet()){
+            ids.put(base + 65  ,entry.getValue());
+            base++;
+        }
+        ids.put(70 , GamePictureManager.apricotTexture); // fruit
+
+
+        ids.put(80 , GamePictureManager.cherrySaplingTexture);
+        ids.put(81 , GamePictureManager.cherryStage1Texture);
+        ids.put(82 , GamePictureManager.cherryStage2Texture);
+        ids.put(83 , GamePictureManager.cherryStage3Texture);
+        ids.put(84 , GamePictureManager.cherryStage4Texture);
+        ids.put(85 , GamePictureManager.cherryStage5WithFruitTexture);
+        base = 0;
+        for(Map.Entry<Season, TextureRegion> entry : GamePictureManager.cherryStage5.entrySet()){
+            ids.put(base + 85  ,entry.getValue());
+            base++;
+        }
+        ids.put(90 , GamePictureManager.cherryTexture);
+
+
+        ids.put(100 , GamePictureManager.mangoSaplingTexture);
+        ids.put(101 , GamePictureManager.mangoStage1Texture);
+        ids.put(102 , GamePictureManager.mangoStage2Texture);
+        ids.put(103 , GamePictureManager.mangoStage3Texture);
+        ids.put(104 , GamePictureManager.mangoStage4Texture);
+        ids.put(105, GamePictureManager.mangoStage5WithFruit);
+        base = 0;
+        for(Map.Entry<Season, TextureRegion> entry : GamePictureManager.mangoStage5.entrySet()){
+            ids.put(base + 105  ,entry.getValue());
+            base++;
+        }
+        ids.put(110, GamePictureManager.mangoTexture);
+
+
+        ids.put(120 , GamePictureManager.orangeSaplingTexture);
+        ids.put(121 , GamePictureManager.orangeStage1Texture);
+        ids.put(122 , GamePictureManager.orangeStage2Texture);
+        ids.put(123 , GamePictureManager.orangeStage3Texture);
+        ids.put(124 , GamePictureManager.orangeStage4Texture);
+        ids.put(125 , GamePictureManager.orangeStage5WithFruit);
+        base = 0;
+        for(Map.Entry<Season, TextureRegion> entry : GamePictureManager.orangeStage5.entrySet()){
+            ids.put(base + 125  ,entry.getValue());
+            base++;
+        }
+        ids.put(130, GamePictureManager.orangeTexture);
+
+        ids.put(140 , GamePictureManager.peachSaplingTexture);
+        ids.put(141 , GamePictureManager.peachStage1Texture);
+        ids.put(142 , GamePictureManager.peachStage2Texture);
+        ids.put(143 , GamePictureManager.peachStage3Texture);
+        ids.put(144 , GamePictureManager.peachStage4Texture);
+        ids.put(145 , GamePictureManager.peachStage5WithFruit);
+        base = 0;
+        for(Map.Entry<Season, TextureRegion> entry : GamePictureManager.peachStage5.entrySet()){
+            ids.put(base + 145  ,entry.getValue());
+            base++;
+        }
+        ids.put(150, GamePictureManager.peachTexture);
+
+        ids.put(160 , GamePictureManager.appleSaplingTexture);
+        ids.put(161 , GamePictureManager.appleStage1Texture);
+        ids.put(162 , GamePictureManager.appleStage2Texture);
+        ids.put(163 , GamePictureManager.appleStage3Texture);
+        ids.put(164 , GamePictureManager.appleStage4Texture);
+        ids.put(165 , GamePictureManager.appleStage5WithFruit);
+        base = 0;
+        for(Map.Entry<Season, TextureRegion> entry : GamePictureManager.appleStage5.entrySet()){
+            ids.put(base + 165  ,entry.getValue());
+            base++;
+        }
+        ids.put(170, GamePictureManager.appleTexture);
+
+        ids.put(180 , GamePictureManager.pomegranateSaplingTexture);
+        ids.put(181 , GamePictureManager.pomegranateStage1Texture);
+        ids.put(182 , GamePictureManager.pomegranateStage2Texture);
+        ids.put(183 , GamePictureManager.pomegranateStage3Texture);
+        ids.put(184 , GamePictureManager.pomegranateStage4Texture);
+        ids.put(185 , GamePictureManager.pomegranateStage5WithFruit);
+        base = 0;
+        for(Map.Entry<Season , TextureRegion> entry : GamePictureManager.pomegranateStage5.entrySet()){
+            ids.put(base + 185  ,entry.getValue());
+            base++;
+        }
+        ids.put(190, GamePictureManager.pomegranateTexture);
+
+
+        ids.put(200 , GamePictureManager.acornTexture);
+        ids.put(201 , GamePictureManager.oakStage1Texture);
+        ids.put(202 , GamePictureManager.oakStage2Texture);
+        ids.put(203 , GamePictureManager.oakStage3Texture);
+        ids.put(204 , GamePictureManager.oakStage4Texture);
+        base = 0;
+        for(Map.Entry<Season , TextureRegion> entry : GamePictureManager.oakStage5.entrySet()){
+            ids.put(base + 204  ,entry.getValue());
+            base++;
+        }
+
+        ids.put(209, GamePictureManager.oakResinTexture);
+
+        ids.put(210 , GamePictureManager.mapleSeedsTexture);
+        ids.put(211 , GamePictureManager.mapleStage1Texture);
+        ids.put(212 , GamePictureManager.mapleStage2Texture);
+        ids.put(213 , GamePictureManager.mapleStage3Texture);
+        ids.put(214 , GamePictureManager.mapleStage4Texture);
+        base = 0;
+        for(Map.Entry<Season , TextureRegion> entry : GamePictureManager.mapleStage5.entrySet()){
+            ids.put(base + 214  ,entry.getValue());
+            base++;
+        }
+        ids.put(219 , GamePictureManager.mapleSyrupTexture);
+
+
+
 
 
 
