5558954b3734f5803777a45b9fc9e3196d1a74b4
amirmahdi <amirmahdifallah8406@gmail.com>
2025-07-29 16:09:04 +0330

    add rendering_player and moveDirection in DTO

--- common/src/main/java/com/stardew/model/PlayerDTO.java
+++ common/src/main/java/com/stardew/model/PlayerDTO.java
@@ -1,19 +1,31 @@
 package com.stardew.model;
 
 public class PlayerDTO {
-    private final int x;
-    private final int y;
+    private final float x;
+    private final float y;
+    private final int direction;
+    private final int energy;
 
-    public PlayerDTO(int x, int y) {
+    public PlayerDTO(float x, float y, int direction, int energy) {
         this.x = x;
         this.y = y;
+        this.direction = direction;
+        this.energy = energy;
     }
 
-    public int getX() {
+    public float getX() {
         return x;
     }
 
-    public int getY() {
+    public float getY() {
         return y;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public int getEnergy() {
+        return energy;
     }
 }
