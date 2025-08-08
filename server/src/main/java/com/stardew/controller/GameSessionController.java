f67437c7ceb97b3ee41d084c993722debcf23c55
amirmahdi <amirmahdifallah8406@gmail.com>
2025-08-08 11:07:25 +0330

    fix some bugs while walking, change initial position, fix a mistake in "FarmFactory"

--- server/src/main/java/com/stardew/controller/GameSessionController.java
+++ server/src/main/java/com/stardew/controller/GameSessionController.java
@@ -159,7 +159,7 @@ public class GameSessionController {
         switch (event) {
             case Moving -> {
                 Player player = game.getPlayer(connection);
-                PlayerController.getInstance().handleMovement(player, game.getMap().getTiles(), message);
+                PlayerController.getInstance().handleMovement(player, game, message);
             }
             case ShowInventory -> {
                 Player p = game.getPlayer(connection);
