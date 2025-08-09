32094a26a2f9619e5b0ad3273e5d57babba6d68a
amirmahdi <amirmahdifallah8406@gmail.com>
2025-08-09 10:18:19 +0330

    add width/height for greenHouse, fix bug for showing image of it

--- client/src/main/java/com/stardew/view/GreenhouseUIManager.java
+++ client/src/main/java/com/stardew/view/GreenhouseUIManager.java
@@ -1,12 +1,10 @@
 package com.stardew.view;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.stardew.models.ClientInfo.LoggedInUser;
 import com.stardew.models.GameAssetManagers.GamePictureManager;
 import com.stardew.network.Message;
 import com.stardew.network.MessageType;
@@ -15,41 +13,45 @@ import com.stardew.network.NetworkManager;
 import java.util.HashMap;
 
 public class GreenhouseUIManager {
-    private static Image image;
+    private static Actor image;
 
     public static void initialize(Stage stage , int gameId) {
+        new Thread(() -> {
+            HashMap<String, Object> body = new HashMap<>();
+            body.put("id", gameId);
+            Message message = new Message(body , MessageType.GREENHOUSE_POSITION_REQUEST);
+            Message response = NetworkManager.getConnection().sendAndWaitForResponse(message , 500);
+            if(response != null && response.getType() == MessageType.GREENHOUSE_POSITION_RESULT) {
+                int x = response.getIntFromBody("x");
+                int y = response.getIntFromBody("y");
+                int width = response.getIntFromBody("width");
+                int height = response.getIntFromBody("height");
 
-        HashMap<String, Object> body = new HashMap<>();
-        body.put("id", gameId);
-        Message message = new Message(body , MessageType.GREENHOUSE_POSITION_REQUEST);
-        Message response = NetworkManager.getConnection().sendAndWaitForResponse(message , 500);
-        if(response != null && response.getType() == MessageType.GREENHOUSE_POSITION_RESULT) {
-            int x = response.getIntFromBody("x");
-            int y = response.getIntFromBody("y");
-            System.out.println("Greenhouse position result: " + x + ", " + y);
-            int xInMaP = x * GamePictureManager.TILE_SIZE;
-            int yInMaP = y * GamePictureManager.TILE_SIZE;
-//            Gdx.app.postRunnable(() -> {
-                image = new Image(new TextureRegionDrawable(GamePictureManager.initialGreenhouse));
-                image.setPosition(xInMaP, yInMaP);
-                stage.addActor(image);
-                image.addListener(new InputListener(){
-                    @Override
-                    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
-                        stage.addActor(new BuildGreenHouseWindow(stage , gameId ));
-                        return true;
-                    }
+                int xInMaP = x * GamePictureManager.TILE_SIZE;
+                int yInMaP = y * GamePictureManager.TILE_SIZE;
+                int widthInMap = width * GamePictureManager.TILE_SIZE;
+                int heightInMap = height * GamePictureManager.TILE_SIZE;
+                Gdx.app.postRunnable(() -> {
+                    image = new Actor();
+                    image.setBounds(xInMaP, yInMaP, widthInMap, heightInMap);
+                    stage.addActor(image);
+                    image.addListener(new InputListener(){
+                        @Override
+                        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
+                            stage.addActor(new BuildGreenHouseWindow(stage , gameId ));
+                            return true;
+                        }
+                    });
                 });
-//            });
 
-
-        }
+            }
+        }).start();
 
 
     }
 
     public static void buildGreenhouse(){
-        image.setDrawable(new TextureRegionDrawable(GamePictureManager.progressiveGreenhouse));
+//        image.setDrawable(new TextureRegionDrawable(GamePictureManager.progressiveGreenhouse));
     }
 
 
