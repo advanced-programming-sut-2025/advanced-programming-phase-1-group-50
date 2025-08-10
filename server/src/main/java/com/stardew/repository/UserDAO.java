aa018d4d47a41b98b9261572c1508e2de7fcb1b6
amirmahdi <amirmahdifallah8406@gmail.com>
2025-08-10 19:04:04 +0330

    create table for saving users_info and some methods added

---
+++ server/src/main/java/com/stardew/repository/UserDAO.java
@@ -0,0 +1,71 @@
+package com.stardew.repository;
+
+import com.stardew.model.gameApp.SecurityQuestion;
+import com.stardew.model.userInfo.Gender;
+import com.stardew.model.userInfo.User;
+import org.sqlite.SQLiteErrorCode;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+    private static UserDAO instance;
+    private final DataSource dataSource;
+
+    private UserDAO(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public static UserDAO getInstance() {
+        if (instance == null) {
+            instance = new UserDAO(DatabaseManager.getInstance().getDataSource());
+        }
+        return instance;
+    }
+
+    public boolean insertUser(User user) throws SQLException {
+        String sql = "INSERT INTO users(username, password_hash, email, gender, security_question, security_answer) VALUES (?, ?, ?, ?, ?, ?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, user.getGender().toString());
+            preparedStatement.setString(5, user.getSecurityQuestion().getQuestion());
+            preparedStatement.setString(6, user.getSecurityQuestion().getAnswer());
+            int rows = preparedStatement.executeUpdate();
+            return rows > 0;
+        } catch (SQLException ex) {
+            if (ex.getErrorCode() == SQLiteErrorCode.SQLITE_CONSTRAINT.code) {
+                return false;
+            }
+            throw ex;
+        }
+    }
+
+    public User findByUsername(String username) throws SQLException {
+        String sql = "SELECT username, password_hash, nickname, email, gender, security_question, security_answer FROM users WHERE username = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, username);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return new User(
+                        resultSet.getString("username"),
+                        resultSet.getString("password_hash"),
+                        resultSet.getString("nickname"),
+                        resultSet.getString("email"),
+                        Gender.valueOf(resultSet.getString("gender")),
+                        new SecurityQuestion(
+                            resultSet.getString("security_question"),
+                            resultSet.getString("security_answer"))
+                    );
+                }
+            }
+        }
+        return null;
+    }
+}
