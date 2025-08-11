f9529589c78ab5cfc57e4e7f65666c72712be2c8
MmdHsMjdn <mohammad.h060684@gmail.com>
2025-08-11 18:56:37 +0330

    adding tag popUp window

---
+++ client/src/main/java/com/stardew/view/Notification/TagNotificationWindow.java
@@ -0,0 +1,43 @@
+package com.stardew.view.Notification;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.stardew.model.Notification.Notification;
+import com.stardew.models.GameAssetManagers.GamePictureManager;
+import com.stardew.view.windows.CloseableWindow;
+
+public class TagNotificationWindow extends CloseableWindow {
+
+    public TagNotificationWindow(Stage stage, Notification notification) {
+        super("Tag Notification", stage);
+
+        setSize(600, 400);
+        Table contentTable = new Table();
+        contentTable.defaults().pad(10);
+
+        Label senderLabel = new Label(notification.getSender() + " tagged you!", GamePictureManager.skin);
+        senderLabel.setColor(Color.GREEN);
+        senderLabel.setFontScale(1.2f);
+
+        Label messageLabel = new Label("Message: " + notification.getMessage(), GamePictureManager.skin);
+        messageLabel.setColor(Color.BLACK);
+        messageLabel.setFontScale(0.85f);
+        messageLabel.setWrap(true);
+
+        contentTable.add(senderLabel).row();
+        contentTable.add(messageLabel).width(300).row();
+
+        add(contentTable).pad(20);
+        pack();
+
+        setPosition(
+            stage.getCamera().position.x - getWidth() / 2,
+            stage.getCamera().position.y - getHeight() / 2
+        );
+
+        stage.addActor(this);
+    }
+}
+
